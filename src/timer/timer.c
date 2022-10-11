cff8acac372b2b597659379c5ce2d2def7e8b253
Qupergo <45945560+Qupergo@users.noreply.github.com>
2022-10-11 16:07:29 +0200

    Cleaned up timer
    Co-Authored-By: Svah-nen <97944478+Svah-nen@users.noreply.github.com>

--- src/timer/timer.c
+++ src/timer/timer.c
@@ -2,53 +2,39 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_tim.h"
 #include "misc.h"
-
-volatile long time_ms;
+#include "timer.h"
 
 void timer_default_irq( void ){
-    time_ms++;
+    timer_ms++;
     TIM_ClearITPendingBit(TIM2, TIM2_IRQn);
 }
 
-void timer_start( void ){
-    //reset ms and start timer
-    time_ms = 0;
-    TIM_Cmd(TIM2, ENABLE);
-}
-
-long timer_stop( void ){
-    //disable clock and return time
-    TIM_Cmd(TIM2, DISABLE);
-    return time_ms;
-}
-
-void timer_init( int ms, int Repeat ){
+void timer_init( int ms ){
     //default timer irq
-     *((int *) 0x2001C0B0) = timer_irq;
-     
-     //Prescaler 42000 - 1 = 0.5 ms
-     //Counter mode up
-     //ms between every interrupt
-     TIM_TimeBaseInitTypeDef TIM_initStructure = {
-         42000 - 1,
-         TIM_CounterMode_Up,
-         2 * ms,
-         TIM_CKD_DIV1
-     };
-     TIM_TimeBaseInit(TIM2, &TIM_initStructure);
-     
-     //One pulse if not repeated interrupts
-     if(!Repeat)
-        TIM_SelectOnePulseMode(TIM2, TIM_OPMode_Single);
-     
-     //Init nvic with highest priorities
-     NVIC_InitTypeDef NVIC_initStructure = {
-         TIM2_IRQn,
-         0,
-         0,
-         ENABLE
-     };
-     NVIC_Init(&NVIC_initStructure);
-     
-     TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    *((int *) 0x2001C0B0) = timer_default_irq;
+    
+    //Prescaler 42000 - 1 = 0.5 ms
+    //Counter mode up
+    //ms between every interrupt
+    TIM_TimeBaseInitTypeDef TIM_initStructure = {
+        42000 - 1,
+        TIM_CounterMode_Up,
+        2 * ms,
+        TIM_CKD_DIV1
+    };
+    TIM_TimeBaseInit(TIM2, &TIM_initStructure);
+    
+    //Init nvic with highest priorities
+    NVIC_InitTypeDef NVIC_initStructure = {
+        TIM2_IRQn,
+        0,
+        0,
+        ENABLE
+    };
+    NVIC_Init(&NVIC_initStructure);
+    
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+    timer_ms = 0;
+    TIM_Cmd(TIM2, ENABLE);
 }
