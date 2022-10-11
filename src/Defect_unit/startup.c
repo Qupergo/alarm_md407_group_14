94dd2f47f43c4d3aa709ad3a5b7546d44e54d61c
el-isaco <isachol@chalmers.se>
2022-10-11 16:51:49 +0200

    Updated Defect unit with CAN

--- src/Defect_unit/startup.c
+++ src/Defect_unit/startup.c
@@ -3,8 +3,10 @@
  *
  */
  
- #include <stdio.h>
- #include <usart.h>
+ #include "usart.h"
+ #include "can.h"
+ #include "stm32f4xx_rcc.h"
+ 
 __attribute__((naked)) __attribute__((section (".start_section")) )
 void startup ( void )
 {
@@ -14,47 +16,68 @@ __asm__ volatile(" BL main\n");					/* call main */
 __asm__ volatile(".L1: B .L1\n");				/* never return */
 }
 
-
-
-void CAN_send(char* p)
-{
-	//Function placeholder for CAN-protocol function
-	print("hello world");
-}
- typedef struct
-{
-	int priority;		// Priority bit 10
-	int type;			// Message type bits 9-6
-	int sender;			// Sender ID bits 5-3
-	int receiver;		// Receiver ID  bits 3-0
-
-} CAN_msg;
-
-void delayms(int i)
-{
-	//delay from TIM
-}
+// timer_ms set to 0 for now, change when implementing timer.c
+int timer_ms = 0;
 
 void main(void)
 {
-	// If only send data via a function for send this will be fine, 
-	// just need to check so it's the right amout of bits sent in the array
-	char jumble[] = {'D','e','f','e','c','t',' ','B','o','a','r','d'};
-//	int i = 8;
-	while(1)
-	{
-		CAN_send(jumble);
-//		delayms(3);
+	// For all units
+	rt_info _defect_rt_info;
+	ls_info _defect_ls_info;
+	tx_can_msg _defect_tx_msg;
+	
+	// To simulate a defect arbitrary numbers are used being not all ones or zeroes 
+	for (int i = 0; i < MAX_UNITS; i++) {
+		_defect_rt_info.transmit_sequence_num[i] = timer_ms*1;
+		_defect_rt_info.recieve_sequence_num[i] = timer_ms*2;
+		_defect_ls_info.is_connected[i] = timer_ms*3;
+		_defect_ls_info.latest_self_lifesign_timestamp = timer_ms*4;
+		_defect_ls_info.recieved_lifesigns[i] = timer_ms*5;
+	}
+	for (int i = 0; i < MAX_RT_FRAMES; i++) {
+		_defect_rt_info.rt_frames[i].is_used = timer_ms*6;
 	}
 	
-	// If send data by using a CAN.struct
-	CAN_msg mumble;
-	mumble.priority	= 0b10000000000;
-	mumble.type		= 0b01111000000;
-	mumble.sender	= 0b00000111000;
-	mumble.receiver	= 0b00000000111;
+	_defect_tx_msg.priority = 'A';
+	_defect_tx_msg.message_type = 'B';
+	_defect_tx_msg.reciever_id = 'C';
+	for(int i = 0; i < 6; i++){
+		_defect_tx_msg.content[i] = 'L'; // Literally just take the L
+	}
 	
-	// Send mumble
-	// Waiting for CAN protocol to finish
+	// For non central units
+	// Send initial alive message to central unit
+	// Imagine this not working on a defect board
+	/*
+	tx_can_msg initial_alive;
+	initial_alive.priority = 1;
+	initial_alive.message_type = NEW_ALIVE_TYPE_ID;
+	initial_alive.content[0] = SELF_TYPE;
+	initial_alive.content[1] = num_sub_units;
+	initial_alive.reciever_id = 0;
+	can_send_message(&_rt_info, &_ls_info, CAN1, initial_alive);
+	unsigned char waiting_for_alive_response = 1;
+	 */
+	
+	
+
+	while (1) {
+		can_send_message(_defect_rt_info, _defect_ls_info, CAN1, txmsg)
+		can_update(&_rt_info, &_ls_info);
+		if (can_receive_message(&_rt_info, &_ls_info, CAN1, &rx_msg)) {
+			switch (rx_msg.message_type) {
+				// For non central units
+				case NEW_ALIVE_RESPONSE_TYPE_ID:
+				if (waiting_for_alive_response) {
+					if (rx_msg.content[0] == SELF_TYPE) {
+						waiting_for_alive_response = 0;
+						self_id = rx_msg.content[1];
+						_rt_info.recieve_sequence_num[rx_msg.sender_id] = 0;
+						_rt_info.transmit_sequence_num[rx_msg.sender_id] = 1;
+					}
+				}
+			}
+		}
+	}
 }
 
