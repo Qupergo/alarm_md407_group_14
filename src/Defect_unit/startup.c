284c35ea04e144fe7d8e28ee1547823138ba25ba
el-isaco <isachol@chalmers.se>
2022-10-12 11:04:47 +0200

    Formatted defect unit

--- src/Defect_unit/startup.c
+++ src/Defect_unit/startup.c
@@ -6,6 +6,7 @@
  #include "usart.h"
  #include "can.h"
  #include "stm32f4xx_rcc.h"
+ #include "timer.h"
  
 __attribute__((naked)) __attribute__((section (".start_section")) )
 void startup ( void )
@@ -16,41 +17,39 @@ __asm__ volatile(" BL main\n");					/* call main */
 __asm__ volatile(".L1: B .L1\n");				/* never return */
 }
 
-// timer_ms set to 0 for now, change when implementing timer.c
-int timer_ms = 0;
-int last_time;
+// Frequency for sending data from defect unit in milliseconds
+int time_between_send = 10;
 
 void main(void)
 {
-	rt_info _defect_rt_info;
-	ls_info _defect_ls_info;
-	tx_can_msg _defect_tx_msg;
+	rt_info defect_rt_info;
+	ls_info defect_ls_info;
+	tx_can_msg defect_tx_msg;
 	
+	timer_init(1);
 	// To simulate a defect arbitrary numbers are used being not all ones or zeroes 
 	for (int i = 0; i < MAX_UNITS; i++) {
-		_defect_rt_info.transmit_sequence_num[i] = timer_ms*1;
-		_defect_rt_info.recieve_sequence_num[i] = timer_ms*2;
-		_defect_ls_info.is_connected[i] = timer_ms*3;
-		_defect_ls_info.latest_self_lifesign_timestamp = timer_ms*4;
-		_defect_ls_info.recieved_lifesigns[i] = timer_ms*5;
+		defect_rt_info.transmit_sequence_num[i] = timer_ms*1;
+		defect_rt_info.recieve_sequence_num[i] = timer_ms*2;
+		defect_ls_info.is_connected[i] = timer_ms*3;
+		defect_ls_info.latest_self_lifesign_timestamp = timer_ms*4;
+		defect_ls_info.recieved_lifesigns[i] = timer_ms*5;
 	}
 	for (int i = 0; i < MAX_RT_FRAMES; i++) {
-		_defect_rt_info.rt_frames[i].is_used = timer_ms*6;
+		defect_rt_info.rt_frames[i].is_used = timer_ms*6;
 	}
 	
-	_defect_tx_msg.priority = 1;
-	_defect_tx_msg.message_type = 1;
-	_defect_tx_msg.reciever_id = 1;
+	defect_tx_msg.priority = 1;
+	defect_tx_msg.message_type = 1;
+	defect_tx_msg.reciever_id = 1;
 	for(int i = 0; i < 6; i++){
-		_defect_tx_msg.content[i] = 'L'; // Literally just take the L
+		defect_tx_msg.content[i] = 'L'; // Literally just take the L
 	}
 
 
 	while (1) {
-		last_time = timer_ms;
-		
-		if(timer_ms > last_time){
-		can_send_message(&_defect_rt_info, &_defect_ls_info, CAN1, _defect_tx_msg);
+		if(timer_ms >= time_between_send){
+		can_send_message(&defect_rt_info, &defect_ls_info, CAN1, defect_tx_msg);
 		}
 	}
 }
