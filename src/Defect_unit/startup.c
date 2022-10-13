af0990ef30357bdd3f56b1b776d5764b0b2cce01
el-isaco <isachol@chalmers.se>
2022-10-13 11:34:47 +0200

    finished defect unit

--- src/Defect_unit/startup.c
+++ src/Defect_unit/startup.c
@@ -3,6 +3,7 @@
  *
  */
  
+ #include "defect_unit.h"
  #include "usart.h"
  #include "can.h"
  #include "stm32f4xx_rcc.h"
@@ -17,39 +18,29 @@ __asm__ volatile(" BL main\n");					/* call main */
 __asm__ volatile(".L1: B .L1\n");				/* never return */
 }
 
-// Frequency for sending data from defect unit in milliseconds
-int time_between_send = 10;
 
 void main(void)
 {
-	rt_info defect_rt_info;
-	ls_info defect_ls_info;
-	tx_can_msg defect_tx_msg;
-	
-	timer_init(1);
-	// To simulate a defect arbitrary numbers are used being not all ones or zeroes 
-	for (int i = 0; i < MAX_UNITS; i++) {
-		defect_rt_info.transmit_sequence_num[i] = timer_ms*1;
-		defect_rt_info.recieve_sequence_num[i] = timer_ms*2;
-		defect_ls_info.is_connected[i] = timer_ms*3;
-		defect_ls_info.latest_self_lifesign_timestamp = timer_ms*4;
-		defect_ls_info.recieved_lifesigns[i] = timer_ms*5;
-	}
-	for (int i = 0; i < MAX_RT_FRAMES; i++) {
-		defect_rt_info.rt_frames[i].is_used = timer_ms*6;
-	}
-	
-	defect_tx_msg.priority = 1;
-	defect_tx_msg.message_type = 1;
-	defect_tx_msg.reciever_id = 1;
-	for(int i = 0; i < 6; i++){
-		defect_tx_msg.content[i] = 'L'; // Literally just take the L
-	}
-
+	int timestamp_of_last_send = 0;
+	timer_init();
 
 	while (1) {
-		if(timer_ms >= time_between_send){
-		can_send_message(&defect_rt_info, &defect_ls_info, CAN1, defect_tx_msg);
+		
+		if((TIME_BETWEEN_SEND_MS + timestamp_of_last_send) <= timer_ms ) {
+			timestamp_of_last_send = timer_ms;
+			// send message with standard id and specify the length of the data
+			CanTxMsg outgoing = {
+			.StdId = 5, // Id is 3 bits meaning the highest possible would be 7
+			.DLC = 8, // 8 is the greatest amount, do not change volume using this, change with frequency instead
+			.RTR = CAN_RTR_DATA,
+			.IDE = CAN_Id_Standard
+			};
+	
+			// fill the data field
+			for(int i = 0; i < length; i++){
+			outgoing.Data[i] = 0;
+			}
+			CAN_Transmit(CANx, outgoing);
 		}
 	}
 }
