7d9293420a3facdf42fd25ab7be3c98d61a95660
Qupergo <45945560+Qupergo@users.noreply.github.com>
2022-10-26 11:40:56 +0200

    Updated central unit menu

--- src/central_unit/startup.c
+++ src/central_unit/startup.c
@@ -32,24 +32,35 @@ void print_menu_options( void ) {
 	USART_Snd_StrLn("8 Restart alarm");
 }
 
+u_info* get_unit_of_type(enum unit_type_id unit_type) {
+	u_info matching_units[MAX_UNITS];
+
+	for (int i = 0; i < MAX_UNITS; i++) {
+		if (units[i].type == unit_type) {
+			matching_units[i] = units[i];
+		}
+	}
+}
+
 // Returns the amount of chars neeeded to execute the command.
-int execute_option(int option, int chars_entered) {
+int execute_option(rt_info* _rt_info, int option, int chars_entered) {
 	switch (option) {
-		case 1: ;// Set a new password
-			if (chars_entered == 4) {
-				*latest_char_buffer.current_password = get_latest_chars_entered(PASSWORD_LENGTH);
+		case 1: // Set a new password
+			if (chars_entered == PASSWORD_LENGTH) {
+				for(int i = 0; i < PASSWORD_LENGTH; i++) {
+					latest_char_buffer.current_password[i] = get_latest_chars_entered(PASSWORD_LENGTH)[i];
+				}
 				return 1;
 			}
 			break;
-		case 2: ;// View alarm status
-			if(chars_entered == 1) {
-				for(int i = 1; i < MAX_UNITS; i++){ // antigen visa info eller skicka can.msg med förfråga om status
-					break;
-				}
-			} break;
-		case 3: ;// Enable door alarm
+		case 2: // View alarm status
+			for(int i = 1; i < MAX_UNITS; i++){ // antigen visa info eller skicka can.msg med förfråga om status
+				continue;
+			}
+			return 1;
+		case 3: // Enable door alarm
 			if (chars_entered == 1) {
-				char door_id = get_latest_chars_entered(1);
+				char door_id = *get_latest_chars_entered(1);
 				tx_can_msg alarm_message_on;
 				alarm_message_on.message_type = MSGID_START_ALARM;
 				alarm_message_on.reciever_id = TYPE_DOOR_UNIT; // Set to a correct id
@@ -59,9 +70,9 @@ int execute_option(int option, int chars_entered) {
 				return 1;
 			}
 			break;
-		case 4: ;// Disable door alarm
+		case 4: // Disable door alarm
 			if (chars_entered == 1) {
-				char door_id = get_latest_chars_entered(1);
+				char door_id = *get_latest_chars_entered(1);
 				tx_can_msg alarm_message_off;
 				alarm_message_off.message_type = MSGID_STOP_ALARM;
 				alarm_message_off.reciever_id = TYPE_DOOR_UNIT; // Set to a correct id
@@ -70,59 +81,81 @@ int execute_option(int option, int chars_entered) {
 				return 1;
 			}
 			break;
-		case 5: ; // Set new time threshold
+		case 5:  // Set new time threshold
 			if (chars_entered > 1) {
-				char* latest_char = get_latest_chars_entered(1);
-				if (*latest_char == 0xD) {
+				char latest_char = *get_latest_chars_entered(1);
+				if (latest_char == 0xD) {
 					char* new_threshold = get_latest_chars_entered(chars_entered);
-						tx_can_msg set_new_threshold;
-						set_new_threshold.message_type = MSGID_SET_DOOR_ALARM_TIME_THRESHOLD;
-						set_new_threshold.reciever_id = TYPE_DOOR_UNIT;
-						set_new_threshold.priority = 1;
-						set_new_threshold.content[0] = new_threshold[2];
-						set_new_threshold.content[1] = new_threshold[3]; // the value of threshold entered must be one digit in this case 
-						set_new_threshold.content[2] = new_threshold[1]; // the first index will be option number;5.
-				}return 1;
+					tx_can_msg set_new_threshold;
+					set_new_threshold.message_type = MSGID_SET_DOOR_ALARM_TIME_THRESHOLD;
+					set_new_threshold.reciever_id = TYPE_DOOR_UNIT;
+					set_new_threshold.priority = 1;
+					int i = 0;
+					while (*new_threshold != 0xD) {
+						set_new_threshold.content[i] = new_threshold[i];
+						i++;
+					}
+					return 1;
+				}
 			}
 			break;
-		case 6: ;// caliberate distance alarm 
-			//if (chars_entered == 1) {
-				tx_can_msg recaliberation;
-				recaliberation.message_type = MSGID_RECALIBERATE;
-				recaliberation.reciever_id = TYPE_SENSOR_UNIT;
-				recaliberation.content[0] = 0; // set a default value for no specific reason
-			break;
-		case 7: ;// set sensitivity for distance sensor
+		case 6: // caliberate distance alarm
+			for (int i = 0; i < MAX_UNITS; i++) {
+				if (units[i].type == TYPE_SENSOR_UNIT) {
+					tx_can_msg msg_recalibrate = {
+						.message_type = MSGID_RECALIBERATE,
+						.reciever_id = units[i].main_id,
+					};
+					can_send_message(_rt_info, CAN1, msg_recalibrate);
+				}
+			}
+			return 1;
+		case 7: // set sensitivity for distance sensor
 			if (chars_entered == 1) {
-				unsigned char sensitivity_value = get_latest_chars_entered(1);
-				tx_can_msg reset_sensitivity;
-				reset_sensitivity.message_type = MSGID_SENSOR_DISTANCE_THRESHOLD;
-				reset_sensitivity.reciever_id = TYPE_SENSOR_UNIT;
-				reset_sensitivity.content[0] = sensitivity_value;
-			}return 1;
+				unsigned char sensitivity_value = *get_latest_chars_entered(1);
+
+				for (int i = 0; i < MAX_UNITS; i++) {
+					if (units[i].type == TYPE_SENSOR_UNIT) {
+						tx_can_msg reset_sensitivity = {
+							.message_type = MSGID_SENSOR_DISTANCE_THRESHOLD,
+							.reciever_id = units[i].main_id,
+							.content[0] = sensitivity_value
+						};
+						can_send_message(_rt_info, CAN1, reset_sensitivity);
+					}
+				}
+				return 1;
+			}
 			break;
-		
-		case 8: ;
-			tx_can_msg re_start_1;
-			tx_can_msg re_start_2;
-				re_start_1.message_type = MSGID_RESET_UNIT;
-				re_start_1.reciever_id = TYPE_SENSOR_UNIT;
-				re_start_2.message_type = MSGID_RESET_UNIT;
-				re_start_2.reciever_id = TYPE_DOOR_UNIT;
+		case 8:
+			// Start at 1 to avoid sending to central unit
+			for (int i = 1; i < MAX_UNITS; i++) {
+				if (units[i].is_used) {
+					tx_can_msg msg_reset_unit = {
+						.priority = 1,
+						.message_type = MSGID_RESET_UNIT,
+						.reciever_id = units[i].main_id,
+					};
+					can_send_message(_rt_info, CAN1, msg_reset_unit);
+				}
+			}
+			
+			return 1;
 	}
 }
 
 void main(void)
 {
 	keypad_init();
-	can_init(CAN1,1);
 	rx_can_msg rx_msg;
     rt_info _rt_info;
     ls_info _ls_info;
-	u_info central_unit;
 	
+	can_init(&_rt_info, &_ls_info, CAN1, 1);
+
 	// call a function in can.c that initialize info for recieving_transmit and life_signals
-	
+	u_info central_unit;
+
     central_unit.is_used = 1;
     central_unit.type = TYPE_CENTRAL_UNIT;
     central_unit.main_id = 0;
@@ -130,20 +163,20 @@ void main(void)
     units[0] = central_unit;
 
 	int choosing_menu_option = 0;
-	unsigned char option;
+	char option;
 	int executing_menu_option = 0;
 	int chars_entered_for_option = 0;
 
 	while (1) {
-        if(1 ) {
+		if (keypad_update()) {
 			if (choosing_menu_option) {
-				option = get_latest_chars_entered(1);
+				option = *get_latest_chars_entered(1);
 				choosing_menu_option = 0;
 				executing_menu_option = 1;
 			}
 			else if (executing_menu_option) {
 				chars_entered_for_option++;
-				if (execute_option(option, chars_entered_for_option)) {
+				if (execute_option(&_rt_info, option, chars_entered_for_option)) {
 					executing_menu_option = 0;
 					chars_entered_for_option = 0;
 				}
@@ -154,6 +187,7 @@ void main(void)
 				choosing_menu_option = 1;
 			}
 		}
+
         can_update(&_rt_info, &_ls_info);
         if (can_receive_message(&_rt_info, &_ls_info, CAN1, &rx_msg)) {
             switch (rx_msg.message_type) {
@@ -175,7 +209,7 @@ void main(void)
                             alive_response.message_type = MSGID_NEW_ALIVE_RESPONSE;
                             alive_response.content[0] = rx_msg.content[0];
                             alive_response.content[1] = i;
-                            can_send_message(&_rt_info, &_ls_info, CAN1, alive_response);
+                            can_send_message(&_rt_info, CAN1, alive_response);
                         }
                     }
                     break;
@@ -184,7 +218,6 @@ void main(void)
 					unsigned char alarm_sender = rx_msg.sender_id;
 					unsigned char time_passed_since_alarm = timer_ms; // maybe as global variable ?
 					USART_Snd_StrLn("ALARM");
-					trigger_alarm();
 					 
 			}
         }
